0c9ca910e82bf55d5e8ad5712d5f248248629aba
WAVEKUB <itsarawut.happy@gmail.com>
2025-06-12 09:06:20 +0700

    product mangement

---
+++ backend/cartcraft/src/main/java/com/cart/cartcraft/CartcraftApplication.java
@@ -0,0 +1,13 @@
+package com.cart.cartcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartcraftApplication.class, args);
+	}
+
+}
